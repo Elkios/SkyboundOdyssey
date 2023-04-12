5bcee8c0dc051c3cd0066df0940f7a2900d0545c
unknnow <48657384+unknnow@users.noreply.github.com>
2023-04-12 15:42:17 +0200

    Deplacement du Systeme de Pattern dans un fichier à part (PatternManager.js)

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import PatternManager from '../managers/PatternManager';
 import ParallaxManager from "../managers/ParallaxManager";
 
 class GameScene extends Phaser.Scene {
@@ -10,6 +11,7 @@ class GameScene extends Phaser.Scene {
     character;
     cursors;
     parallaxManager = new ParallaxManager(this, 'darkForest', 10);
+    patternManager = new PatternManager(this);
 
     preload() {
         // Load character spritesheet
@@ -27,37 +29,15 @@ class GameScene extends Phaser.Scene {
             frameHeight: 639
         });
 
-        // Obstacles
-        this.load.image("spike1", "assets/obstacles/spike/spike_1.png");
-        this.load.image("woodFenceSingle", "assets/obstacles/woodfence/single.png");
-        this.load.image("woodFenceMultiple", "assets/obstacles/woodfence/multiple.png");
-
-        // Ghost
-        for (let i = 1; i <= 30; i++) {
-            this.load.image(`ghost_frame_${i}`, `assets/obstacles/ghost/animation/skeleton-animation_${i}.png`);
-        }
-
-        // Lightning Trap
-        this.load.spritesheet("lightning_trap", "assets/obstacles/lightningTrap/lightningTrap.png", {
-            frameWidth: 96,
-            frameHeight: 89,
-        });
-
-        // Gold Coins
-        this.load.image("goldCoin", "assets/coins/gold/coin.png");
-        for (let i = 1; i <= 10; i++) {
-            this.load.image(`gold_coin_frame_${i}`, `assets/coins/gold/animation/Gold_${i}.png`);
-        }
-
         this.parallaxManager.preload();
         this.load.audio("main", "assets/musics/main.mp3");
-
-        // Audio piece
-        this.load.audio("coin_pickup", "assets/coins/gold/sound/coin_pickup.mp3");
 
         // Audio footstep & jetpack
         this.load.audio("footsteps", "assets/player/sound/footsteps.mp3");
         this.load.audio("jetpack", "assets/player/sound/jetpack.mp3");
+
+        // Load pattern Manager
+        this.patternManager.preload();
     }
 
     create() {
@@ -103,69 +83,21 @@ class GameScene extends Phaser.Scene {
         // Contrôles
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.add.existing(this.character);
-
-        // Score/Coins
-        this.score = 0;
-        this.scoreText = this.add.text(10, 10, "Score: 0", {
-            fontSize: "24px",
-            color: "#ffffff",
-        });
-        this.gameOver = false;
-
-        // Obstacles
-        this.obstacles = this.physics.add.group();
-        this.coins = this.physics.add.group();
-
-        // Animation ghost
-        this.anims.create({
-            key: "ghost_animation",
-            frames: Array.from({length: 30}, (_, i) => ({
-                key: `ghost_frame_${i + 1}`,
-                frame: 0,
-            })),
-            frameRate: 10,
-            repeat: -1,
-        });
-
-        // Animation lightning trap
-        this.anims.create({
-            key: "lightning_trap_animation",
-            frames: this.anims.generateFrameNumbers("lightning_trap", {start: 0, end: 21}),
-            frameRate: 10,
-            repeat: -1,
-        });
-
-
-        // Animation piece
-        this.anims.create({
-            key: "coin_rotate",
-            frames: Array.from({ length: 10}, (_, i) => ({
-                key: `gold_coin_frame_${i + 1}`,
-                frame: 0,
-            })),
-            frameRate: 10,
-            repeat: -1,
-        });
-
-        // Audio piece
-        this.coinPickupSound = this.sound.add("coin_pickup");
-
         // Audio footstep & jetpack
         this.footstepsSound = this.sound.add("footsteps");
         this.jetpackSound = this.sound.add("jetpack");
         this.jetpackSound.setVolume(0.2);
 
-        this.time.addEvent({
-            delay: 6000,
-            callback: this.spawnPattern,
-            callbackScope: this,
-            loop: true,
-        });
+        // Pattern Manager
+        this.patternManager.create();
+
+        this.add.existing(this.character);
     }
 
     update(time, delta) {
         this.parallaxManager.update();
+        this.patternManager.update();
+
         // Gérer les mises à jour du jeu à chaque image (entrées utilisateur, collisions, etc.) ici
         // Contrôles du personnage
         if (this.cursors.space.isDown) {
@@ -203,150 +135,6 @@ class GameScene extends Phaser.Scene {
                 this.jetpackSound.stop();
             }
         }
-
-        // Collision obstacle
-        this.physics.overlap(this.character, this.obstacles, () => {
-            if (!this.gameOver) {
-                this.gameOver = true;
-                this.music.stop();
-                // Implémentez ici la logique de fin de partie
-            }
-        }, null, this);
-
-        // Collision score/pieces
-        this.physics.overlap(this.character, this.coins, (character, coin) => {
-            if (!this.gameOver) {
-                coin.destroy();
-                this.score += 10; // Ajoutez les points pour chaque pièce collectée
-                this.scoreText.setText("Score: " + this.score);
-                this.coinPickupSound.play(); // Jouez le son de collecte de pièce
-            }
-        }, null, this);
-    }
-
-    spawnPattern() {
-        // X => Horizontal | Y => Vetical
-        const patterns = [
-            [
-                { type: "lightningTrap", x: 0, y: 100 },
-                { type: "coin", x: -100, y: 300 },
-                { type: "coin", x: 0, y: 300 },
-                { type: "coin", x: 100, y: 300 },
-                { type: "lightningTrap", x: 0, y: window.innerHeight / 2 },
-                { type: "coin", x: -100, y: window.innerHeight - 300 },
-                { type: "coin", x: 0, y: window.innerHeight - 300 },
-                { type: "coin", x: 100, y: window.innerHeight - 300 },
-                { type: "lightningTrap", x: 0, y: window.innerHeight - 100 },
-            ],
-            [
-                { type: "ghost", x: 0, y: 100 },
-                { type: "ghost", x: 0, y: 275 },
-                { type: "coin", x: 0, y: window.innerHeight / 2 },
-                { type: "ghost", x: 0, y: window.innerHeight - 275 },
-                { type: "ghost", x: 0, y: window.innerHeight - 100 },
-            ],
-            [
-                { type: "spike1", x: 0, y: window.innerHeight - 30 },
-                { type: "coin", x: 0, y: window.innerHeight - 200 },
-                { type: "spike1", x: 150, y: window.innerHeight - 30 },
-                { type: "coin", x: 150, y: window.innerHeight - 200 },
-                { type: "coin", x: 300, y: window.innerHeight - 100 },
-                { type: "spike1", x: 500, y: window.innerHeight - 30 },
-                { type: "coin", x: 500, y: window.innerHeight - 200 },
-                { type: "spike1", x: 650, y: window.innerHeight - 30 },
-                { type: "coin", x: 650, y: window.innerHeight - 200 },
-            ],
-            [
-                { type: "woodFenceSingle", x: -100, y: window.innerHeight - 200, rotation: -45 },
-                { type: "woodFenceSingle", x: 0, y: window.innerHeight - 100, rotation: 135 },
-                { type: "woodFenceSingle", x: -100, y: 200, rotation: -135 },
-                { type: "woodFenceSingle", x: 0, y: 100, rotation: 45 },
-
-                { type: "coin", x: 200 , y: (window.innerHeight / 2) - 150 },
-                { type: "coin", x: 400 , y: (window.innerHeight / 2) - 150 },
-                { type: "coin", x: 600 , y: (window.innerHeight / 2) - 150 },
-                { type: "lightningTrap", x: 200, y: window.innerHeight / 2, rotation: 90 },
-                { type: "lightningTrap", x: 400, y: window.innerHeight / 2, rotation: 90 },
-                { type: "lightningTrap", x: 600, y: window.innerHeight / 2, rotation: 90 },
-                { type: "coin", x: 200 , y: (window.innerHeight / 2) + 150 },
-                { type: "coin", x: 400 , y: (window.innerHeight / 2) + 150 },
-                { type: "coin", x: 600 , y: (window.innerHeight / 2) + 150 },
-
-
-                { type: "woodFenceSingle", x: 800, y: 200, rotation: 135 },
-                { type: "woodFenceSingle", x: 700, y: 100, rotation: -45 },
-                { type: "woodFenceSingle", x: 800, y: window.innerHeight - 200, rotation: 45 },
-                { type: "woodFenceSingle", x: 700, y: window.innerHeight - 100, rotation: -135 },
-            ],
-            [
-                { type: "woodFenceMultiple", x: 0, y: 75, rotation: 180 },
-                { type: "coin", x: 0, y: 250 },
-                { type: "woodFenceMultiple", x: 0, y: window.innerHeight - 75 },
-                { type: "coin", x: 0, y: window.innerHeight - 250 },
-
-                { type: "woodFenceSingle", x: 400 - 72, y: window.innerHeight / 2, rotation: -90 },
-                { type: "woodFenceSingle", x: 400 + 72, y: window.innerHeight / 2, rotation: 90 },
-                { type: "coin", x: 400, y: 150 },
-                { type: "coin", x: 400, y: window.innerHeight - 150 },
-                { type: "woodFenceSingle", x: 400, y: (window.innerHeight / 2) - 72 },
-                { type: "woodFenceSingle", x: 400, y: (window.innerHeight / 2) + 72, rotation: 180 },
-
-                { type: "woodFenceMultiple", x: 800, y: 75, rotation: 180 },
-                { type: "coin", x: 800, y: 250 },
-                { type: "woodFenceMultiple", x: 800, y: window.innerHeight - 75 },
-                { type: "coin", x: 800, y: window.innerHeight - 250 },
-            ],
-        ];
-
-        const pattern = Phaser.Utils.Array.GetRandom(patterns);
-        pattern.forEach((item) => {
-            this.createGameObject(item);
-        });
-    }
-
-    createGameObject(item) {
-        let gameObject;
-
-        switch (item.type) {
-            case "spike1":
-                gameObject = this.obstacles.create(this.scale.width + item.x, item.y, "spike1");
-                gameObject.setScale(0.2);
-                break;
-            case "woodFenceSingle":
-                gameObject = this.obstacles.create(this.scale.width + item.x, item.y, "woodFenceSingle");
-                gameObject.setScale(0.4);
-                break;
-            case "woodFenceMultiple":
-                gameObject = this.obstacles.create(this.scale.width + item.x, item.y, "woodFenceMultiple");
-                gameObject.setScale(0.4);
-                break;
-            case "ghost":
-                gameObject = this.obstacles.create(this.scale.width + item.x, item.y, "ghost_frame_1");
-                gameObject.anims.play("ghost_animation");
-                gameObject.setScale(0.25);
-                break;
-            case "lightningTrap":
-                gameObject = this.obstacles.create(this.scale.width + item.x, item.y, "lightning_trap");
-                gameObject.anims.play("lightning_trap_animation");
-                gameObject.setScale(1.5);
-                break;
-            case "coin":
-                gameObject = this.coins.create(this.scale.width + item.x, item.y, "goldCoin");
-                gameObject.anims.play("coin_rotate");
-                gameObject.setScale(0.1);
-                break;
-        }
-
-        if (gameObject) {
-            gameObject.setVelocityX(-200);
-            gameObject.body.allowGravity = false;
-
-            if (item.rotation) {
-                gameObject.angle = item.rotation;
-            }
-        }
-
-        return gameObject;
     }
 }
 
