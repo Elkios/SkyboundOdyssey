2b24be540797e7c578e532c0929a7d22e2c9397a
unknnow <48657384+unknnow@users.noreply.github.com>
2023-04-12 11:08:22 +0200

    Ajout pattern obstacle/coin & Asset coins/obstacles

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -27,8 +27,25 @@ class GameScene extends Phaser.Scene {
             frameHeight: 639
         });
 
+        // Obstacles
+        this.load.image("obstacle", "assets/obstacle/obstacle.png");
+
+        // Ghost
+        for (let i = 1; i <= 30; i++) {
+            this.load.image(`ghost_frame_${i}`, `assets/obstacles/ghost/animation/skeleton-animation_${i}.png`);
+        }
+
+        // Gold Coins
+        this.load.image("goldCoin", "assets/coins/gold/coin.png");
+        for (let i = 1; i <= 10; i++) {
+            this.load.image(`gold_coin_frame_${i}`, `assets/coins/gold/animation/Gold_${i}.png`);
+        }
+
         this.parallaxManager.preload();
         this.load.audio("main", "assets/musics/main.mp3");
+
+        // Audio piece
+        this.load.audio("coin_pickup", "assets/coins/gold/sound/coin_pickup.mp3");
     }
 
     create() {
@@ -75,6 +92,50 @@ class GameScene extends Phaser.Scene {
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.add.existing(this.character);
+
+        // Score/Coins
+        this.score = 0;
+        this.scoreText = this.add.text(10, 10, "Score: 0", {
+            fontSize: "24px",
+            color: "#ffffff",
+        });
+        this.gameOver = false;
+
+        // Obstacles
+        this.obstacles = this.physics.add.group();
+        this.coins = this.physics.add.group();
+
+        // Aniamtion ghost
+        this.anims.create({
+            key: "ghost_animation",
+            frames: Array.from({length: 30}, (_, i) => ({
+                key: `ghost_frame_${i + 1}`,
+                frame: 0,
+            })),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        // Animation piece
+        this.anims.create({
+            key: "coin_rotate",
+            frames: Array.from({ length: 10}, (_, i) => ({
+                key: `gold_coin_frame_${i + 1}`,
+                frame: 0,
+            })),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        // Audio piece
+        this.coinPickupSound = this.sound.add("coin_pickup");
+
+        this.time.addEvent({
+            delay: 4000, // Toutes les 2 secondes
+            callback: this.spawnPattern,
+            callbackScope: this,
+            loop: true,
+        });
     }
 
     update(time, delta) {
@@ -102,6 +163,65 @@ class GameScene extends Phaser.Scene {
                 );
             }
         }
+
+        // Collision obstacle
+        this.physics.overlap(this.character, this.obstacles, () => {
+            if (!this.gameOver) {
+                console.log("Collision avec un obstacle");
+                this.gameOver = true;
+                this.music.stop();
+                // Implémentez ici la logique de fin de partie
+            }
+        }, null, this);
+
+        // Collision score/pieces
+        this.physics.overlap(this.character, this.coins, (character, coin) => {
+            if (!this.gameOver) {
+                console.log("Collision avec une pièce");
+                coin.destroy();
+                this.score += 10; // Ajoutez les points pour chaque pièce collectée
+                this.scoreText.setText("Score: " + this.score);
+                this.coinPickupSound.play(); // Jouez le son de collecte de pièce
+            }
+        }, null, this);
+    }
+
+    spawnPattern() {
+        const patterns = [
+            // pattern 1
+            [
+                { type: "ghost", x: 200, y: 100 },
+                { type: "coin", x: 250, y: 150 },
+            ],
+            // pattern 2
+            [
+                { type: "ghost", x: 300, y: 200 },
+                { type: "coin", x: 350, y: 250 },
+            ],
+            // Ajoutez d'autres patterns ici
+        ];
+
+        const pattern = Phaser.Utils.Array.GetRandom(patterns);
+        pattern.forEach((item) => {
+            if (item.type === "obstacle") {
+                const obstacle = this.obstacles.create(this.scale.width + item.x, item.y, "obstacle");
+                obstacle.setVelocityX(-200);
+                obstacle.body.allowGravity = false;
+                obstacle.setScale(0.25);
+            } else if (item.type === "ghost") {
+                const ghost = this.obstacles.create(this.scale.width + item.x, item.y, "ghost_frame_1");
+                ghost.setVelocityX(-200);
+                ghost.body.allowGravity = false;
+                ghost.anims.play("ghost_animation");
+                ghost.setScale(0.25);
+            } else if (item.type === "coin") {
+                const coin = this.coins.create(this.scale.width + item.x, item.y, "goldCoin");
+                coin.setVelocityX(-200);
+                coin.body.allowGravity = false;
+                coin.anims.play("coin_rotate");
+                coin.setScale(0.1);
+            }
+        });
     }
 }
 
